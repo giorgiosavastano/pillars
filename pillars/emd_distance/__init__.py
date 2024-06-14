4603e5fbd8b52d3410c87c7f4ce3cb3a820d6f98
Giorgio Savastano <giorgiosavastano@Giorgios-MBP-2.fritz.box>
2024-06-14 15:33:08 +0200

    working

--- pillars/emd_distance/__init__.py
+++ pillars/emd_distance/__init__.py
@@ -5,6 +5,8 @@ from pillars import (
     compute_emd,
     compute_emd_bulk,
     compute_emd_bulk_par,
+    emd_classify,
+    emd_classify_bulk,
     euclidean_rdist,
     euclidean_rdist_parallel,
 )
