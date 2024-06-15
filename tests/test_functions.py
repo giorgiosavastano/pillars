3e7b0c202662b954b680f323f4078df851431785
Giorgio Savastano <giorgiosavastano@Giorgios-MBP-2.fritz.box>
2024-06-15 10:35:24 +0200

    added tests

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -1,4 +1,5 @@
 import numpy as np
+from pytest import approx, raises
 
 # from netCDF4 import Dataset
 from scipy.optimize import linear_sum_assignment
@@ -10,6 +11,67 @@ from pillars.emd_distance import (
     compute_euclidean_distance,
     compute_earth_movers_distance_2d,
 )
+
+
+def test_euclidean_distance_small_input():
+    small_input1 = np.array([[0.1, 0.2], [0.3, 0.4]])
+    small_input2 = np.array([[0.5, 0.6], [0.7, 0.8]])
+    result = compute_euclidean_distance(small_input1, small_input2, parallel=False)
+    expected = cdist(small_input1, small_input2)
+    assert np.allclose(result, expected, rtol=1e-17)
+
+
+def test_euclidean_distance_large_input():
+    large_input1 = np.random.random((100, 100))
+    large_input2 = np.random.random((100, 100))
+    result = compute_euclidean_distance(large_input1, large_input2, parallel=True)
+    expected = cdist(large_input1, large_input2)
+    assert np.allclose(result, expected, rtol=1e-17)
+
+
+def test_parallel_non_parallel_consistency():
+    test_input1 = np.random.random((20, 20))
+    test_input2 = np.random.random((20, 20))
+    result_non_parallel = compute_euclidean_distance(
+        test_input1, test_input2, parallel=False
+    )
+    result_parallel = compute_euclidean_distance(
+        test_input1, test_input2, parallel=True
+    )
+    assert np.allclose(result_non_parallel, result_parallel, rtol=1e-17)
+
+
+def test_performance():
+    large_input1 = np.random.random((500, 500))
+    large_input2 = np.random.random((500, 500))
+    import time
+
+    start_time = time.time()
+    compute_euclidean_distance(large_input1, large_input2, parallel=True)
+    elapsed_time_parallel = time.time() - start_time
+
+    start_time = time.time()
+    compute_euclidean_distance(large_input1, large_input2, parallel=False)
+    elapsed_time_non_parallel = time.time() - start_time
+
+    assert elapsed_time_parallel < elapsed_time_non_parallel
+
+
+def test_emd_identical():
+    identical_imgs = np.array([[1, 2], [3, 4]], dtype=np.float64)
+    expected = 0
+    # Assuming compute_emd expects a specific format, convert before passing
+    actual = compute_earth_movers_distance_2d(identical_imgs, identical_imgs)
+    assert np.allclose([expected], [actual], rtol=1e-17)
+
+
+def test_emd_classify_single_marker():
+    rng = np.random.default_rng()
+    img_to_classify = rng.random((17, 11))
+    single_img_marker = rng.random((2, 17, 11))  # Ensure there is more than one marker
+    n_classes = 1  # Request fewer classes than markers
+    emd_classes = emd_classify(img_to_classify, single_img_marker, n_classes)
+    assert len(emd_classes) == n_classes
 
 
 def compute_earth_mover_dist(first, second):
